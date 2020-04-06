eeca0b7abd62044b1356138d7e5f762de04d722b
franciscoliu <zheyuanliu@brandeis.edu>
2020-04-06 00:07:21 -0400

    still more to work on...

--- hangman_jupyter.py
+++ hangman_jupyter.py
@@ -1,11 +1,13 @@
 import random
+def generate_random_word(word):
+    return list(word)
+words= 'happy brandeis alpha quarantine coronavirus zoom computerscience programming'
+word = generate_random_word(words)
 def play_hangman():
     want_to_play = True
-    words= list('happy', 'brandeis', 'alpha', 'quarantine','coronavirus','zoom','computerscience','programming')
     while (want_to_play):
         guessed_letters = []
         guesses_left = 6
-        word = random.choice(words)
         letter = input('Please choose a letter')
         done = False
         while not done:
@@ -19,12 +21,18 @@ def play_hangman():
             else:
                 guessed_letters.append(letter)##add letter to guessed letters
                 print('Congrats!! The letter you guessed is in the word')##tell user the letter is in the word
-            if "all the letters in the word have been guessed":
-                "set done to be true and tell the user they won!"
+            if guessed_letters in word: ##all the letters in the word have been guessed
+                print('Congrats!! You found the word!!')
+                done ##set done to be true and tell the user they won!
             elif guesses_left==0:##the number of guesses left is zero
                 print('Sorry, you have used up all your attempts. You lost.')
                 done ##set done to be true and tell the user they lost!
             else:
-                "print the word with a dash for each letter not in guessed_letters"
-                letter = "ask the user for another letter"
-        want_to_play = "ask the user if they want to play another game..."
+                print('The word is', word,'-----',letter not in word)##print the word with a dash for each letter not in guessed_letters
+                letter =input('Please enter another letter.') ##ask the user for another letter
+        want_to_play = input('Do you want to play another game? Y/N')##ask the user if they want to play another game...
+        if want_to_play=='Y':
+            continue
+        else:
+            break
+play_hangman()
